58e8d7d85cd7ac41763dc4690d3271c814aa90e2
Nick Sharp <nick.sharp@urbanmassage.com>
2019-02-08 17:09:10 +0000

    making it work

--- __init__.py
+++ __init__.py
@@ -4,6 +4,7 @@ import urllib2
 from modules import cbpi
 from modules.core.hardware import SensorActive
 from modules.core.props import Property
+import json
 
 PINS = {
     "Bubbles per minute": "v102",
@@ -28,24 +29,43 @@ class PlaatoSensor(SensorActive):
     api_key = Property.Text("Api Key", configurable=True)
     pin = Property.Select(
         "Pin", options=pins, description="Select which metric you want to listen for")
-    unit = Property.Select("Unit", options=["°C", "°F"])
     refresh_time = Property.Text(
         "Refresh Time", default_value="5", configurable=True)
 
     def get_unit(self):
-        return self.unit
+        if self.pin == "Temperature":
+            return self.get("v108")
+        elif self.pin == "Volume":
+            return self.get("v109")
+        elif self.pin == "Bubbles per minute":
+            return "bpm"
+        elif self.pin == "ABV":
+            return "%"
+        else:
+            return ""
 
     def execute(self):
         while self.is_running():
             pin_value = PINS[self.pin]
             refresh = float(self.refresh_time)
-            url = "{0}/{1}/get/{2}".format(
-                plaato_base_url, self.api_key, pin_value)
-            print(url)
-            # contents = urllib2.urlopen(url).read()
-            # print(contents)
-            self.data_received(2)
+            response = self.get(pin_value)
+
+            self.data_received(response)
             self.api.socketio.sleep(refresh)
 
-        # contents = urllib2.urlopen(url).read()
-        # print(contents)
+    def get(self, pin):
+        url = "{0}/{1}/get/{2}".format(plaato_base_url, self.api_key, pin)
+        try:
+            response = urllib2.urlopen(url)
+        except urllib2.HTTPError, error:
+            err = error.read()
+            print err
+        else:
+
+            content = response.read()
+            j = json.loads(content)
+
+            if type(j) == list:
+                return j[0]
+            else:
+                return j
